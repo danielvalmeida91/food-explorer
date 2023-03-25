a31a78fceb12bd20e3072e2bdf9b8389f7606976
danielvalmeida91 <danielvalmeida91@gmail.com>
2023-03-25 08:12:19 -0300

    Prepare for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,6 @@
 require('express-async-errors')
+require('dotenv/config')
+
 const AppError = require('./utils/AppError')
 const uploadConfig = require('./config/upload')
 
@@ -30,7 +32,7 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.SERVER_PORT || 3000
 app.listen(PORT, () => {
-  console.log('Server is running')
+  console.log('Server is running on port ' + PORT)
 })
