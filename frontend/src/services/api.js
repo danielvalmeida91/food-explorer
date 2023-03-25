6b32c65206fb4ea1bd5b7bd3db2abfe6419d1568
danielvalmeida91 <danielvalmeida91@gmail.com>
2023-03-25 13:16:45 -0300

    Adjust api address

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
-  // baseURL: 'https://foodexplorer-danielvalmeida91.netlify.app/'
+  // baseURL: 'http://localhost:3333'
+  baseURL: 'https://foodexplorer-danielvalmeida91.netlify.app/'
 })
